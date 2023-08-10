f42e5a23fb767e65472c673a86e7bed8f47bd5da
agtagaruma <agtagaruma05@gmail.com>
2023-08-10 08:45:45 +0700

    commit

---
+++ litho/src/Components/ProgressBar/ProgressBarData.jsx
@@ -0,0 +1,59 @@
+const ProgressBarData01 = [
+    {
+        title:"Communication",
+        value:"90",
+    },
+    {
+        title:"Problem-Solving",
+        value:"95",
+    },
+    {
+        title:"Creativity",
+        value:"95",
+    },
+    {
+        title:"Client/Student Interaction",
+        value:"85",
+    },
+    {
+        title:"Technology/Software Proficiency",
+        value:"85",
+    },
+    {
+        title:"Planning and Strategy",
+        value:"85",
+    },
+    {
+        title:"Project/Task Management",
+        value:"90",
+    },
+    {
+        title:"Professional Development",
+        value:"90",
+    },
+    {
+        title:"Adaptability and Flexibility",
+        value:"95",
+    },
+    {
+        title:"Attention to Detail",
+        value:"90",
+    },
+]
+
+const ProgressBarData02 = [
+    {
+        title:"DEVELOPMENT",
+        value:"95",
+    },
+    {
+        title:"PROMOTION",
+        value:"85",
+    },
+    {
+        title:"BRANDING",
+        value:"75",
+    },
+]
+
+export { ProgressBarData01 , ProgressBarData02}
